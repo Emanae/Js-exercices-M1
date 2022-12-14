b623febfa7cec80743a708a6a5d84cd3909b2d06
Emanae <minigrillon@gmail.com>
2022-12-14 21:52:51 +0100

    autocomplete refactoring

--- projects/autocomplete/index.js
+++ projects/autocomplete/index.js
@@ -2,6 +2,7 @@ const url = "https://geo.api.gouv.fr"
 const input = document.getElementById('geo');
 input.addEventListener('input', getSuggestions);
 const suggestionContainer = document.getElementById('suggestions');
+const choiceContainer = document.getElementById('searchPlace');
 
 async function getSuggestions() {
     const inputValue = document.getElementById("geo").value;
@@ -10,24 +11,29 @@ async function getSuggestions() {
     const regions = await getUrlDatas(url + `/regions?nom=${inputValue}`);
     const municipalitiesObjects = municipalities.map(municipality => {
         return {
-            type: "municipality",
+            type: "commune",
             nom: municipality.nom,
             population: municipality.population,
-            codeDepartement: municipality.codeDepartement
+            codeDepartement: municipality.codeDepartement,
+            codesPostaux: `${municipality.codesPostaux.flat()}`,
         }
-    })
-        .sort((a, b) => a.population - b.population);
+    }).sort((a, b) => a.population - b.population);
+
     const regionsdepartementsObjects = regions.map(region => {
         return {
             type: "region",
             nom: region.nom,
+            codeRegion: region.code,
         }
     }).concat(departements.map(departement => {
         return {
             type: "departement",
             nom: departement.nom,
+            code: departement.code,
+            codeRegion: departement.codeRegion
         }
     })).sort((a, b) => a.nom - b.nom)
+
     const allSuggestions = regionsdepartementsObjects.concat(municipalitiesObjects);
     displaySuggestion(allSuggestions);
 }
@@ -36,41 +42,29 @@ function displaySuggestion(suggestions) {
     suggestionContainer.innerHTML = '';
     suggestions.forEach(suggestion => {
         const li = document.createElement('option');
-        li.textContent = suggestion.type === "municipality" ? `${suggestion.codeDepartement} - ${suggestion.nom}   ` : suggestion.nom;
-        li.value = suggestion.type === "municipality" ? `${suggestion.codeDepartement} - ${suggestion.nom}   ` : suggestion.nom;
+        li.textContent = suggestion.type === "commune" ? `${suggestion.codeDepartement} - ${suggestion.nom}   ` : suggestion.nom;
+        li.value = suggestion.type === "commune" ? `${suggestion.codeDepartement} - ${suggestion.nom}   ` : suggestion.nom;
         li.className = suggestion.type;
         suggestionContainer.append(li);
         li.addEventListener('click', () => displayChoice(suggestion));
     });
 }
 function displayChoice(choice) {
-    document.body.innerHTML = '';
+    suggestionContainer.innerHTML = '';
+    choiceContainer.innerHTML = '';
     const title = document.createElement('h1');
-    title.textContent = "Vous avez choisi :";
-    const h2 = document.createElement('h2');
-    h2.textContent = choice.nom;
-    document.body.append(title);
-    document.body.append(h2);
+    title.textContent = `Vous avez choisi :    ${choice.nom}`;
+    choiceContainer.append(title);
 
-
+    Object.keys(choice).forEach((key) => {
+        const p = document.createElement('p');
+        p.textContent = `${key} : ${choice[key]}`;
+        choiceContainer.append(p);
+    })
 }
-// recuperation de ce qu'il y a dans l'input
-// get url comm + dep + region 
-// création de objet 
-// sort pour commune en foction population 
-// reunion en une ule lit et sort en fonction du nom 
-// affichage de suggestions
-//  class en fonction du type 
-// affichage du  code si commune
+
 async function getUrlDatas(url) {
     const response = await fetch(url)
     const data = await response.json();
     return data;
-}
-async function clearAndSetDatas() {
-
-    const promises = getUrlDatas(url)
-    const persoInfos = await promises;
-    console.log(promises)
-
 }
